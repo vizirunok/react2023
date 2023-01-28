e7ceda9df966f42c8e1ee0cfa1d6c60a1c5f8070
vizirunok <andry.maistrenko@gmail.com>
2023-01-28 15:52:29 +0200

    full

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
                 <Route path={'/register'} element={<RegisterPage/>}/>
                 <Route path={'/login'} element={<LoginPage/>}/>
                 <Route path={'/cars'} element={<CarsPage/>}/>
-                {/*<span></span>*/}
             </Route>
         </Routes>
     );
