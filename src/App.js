66655fa6dfe726af1513389b1af371b2c2d8d564
vizirunok <andry.maistrenko@gmail.com>
2023-01-28 14:30:57 +0200

    new pr

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
                 <Route path={'/login'} element={<LoginPage/>}/>
                 <Route path={'/cars'} element={<CarsPage/>}/>
             </Route>
+            {/*<span></span>*/}
         </Routes>
     );
 }
