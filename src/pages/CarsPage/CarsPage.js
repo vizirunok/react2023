44571b893eec74a2820ec2b750e52685450ead5f
vizirunok <andry.maistrenko@gmail.com>
2023-01-31 15:02:25 +0200

    full

--- src/pages/CarsPage/CarsPage.js
+++ src/pages/CarsPage/CarsPage.js
@@ -2,20 +2,40 @@ import {CarsForm} from "../../components";
 import {Cars} from "../../components/Cars/Cars";
 import {useEffect, useState} from "react";
 import {CarService} from "../../services";
+import {useSearchParams} from "react-router-dom";
 
 
 const CarsPage = () => {
 
     const [cars, setCars] = useState([]);
 
+    const [prev, setPrev] = useState(null);
+    const [next, setNext] = useState(null);
+    const [query,setQuery] = useSearchParams({page: '1'})
+
     useEffect(() => {
-        CarService.getAll().then(({data:res})=> setCars(res.items))
-    }, [])
+        CarService.getAll(query.get('page')).then(({data: res}) => {
+            setCars(res.items);
+            setPrev(res.prev);
+            setNext(res.next)
+        })
+    }, [query]);
+
+
+    const prevPage = () => {
+        setQuery(value=>({page: value.get('page') - 1}))
+    };
+
+    const nextPage = () => {
+        setQuery(value=>({page: +value.get('page') + 1}))
+    };
 
     return (
         <div>
             <CarsForm/>
             <hr/>
+            <button disabled={!prev} onClick={prevPage}>prev</button>
+            <button disabled={!next} onClick={nextPage}>next</button>
             <Cars cars={cars}/>
         </div>
     );
--- src/services/car.service.js
+++ src/services/car.service.js
@@ -2,7 +2,7 @@ import {axiosInstance} from "./axios.service";
 import {urls} from "../configs";
 
 const CarService = {
-    getAll: () => axiosInstance.get(urls.cars),
+    getAll: (page=1) => axiosInstance.get(urls.cars, {params:{page}}),
     create: (car) => axiosInstance.post(urls.cars, car),
     addPhoto: (id, photo) => axiosInstance.patch(`${urls.cars}/${id}`, photo),
 };
